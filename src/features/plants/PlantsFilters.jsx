09002e12f37621506dde664334516590ee872ea5
Alba <albamrquz3@gmail.com>
2024-01-02 23:19:32 +0100

    url params for filters

--- src/features/plants/PlantsFilters.jsx
+++ src/features/plants/PlantsFilters.jsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 import Button from "../../ui/Button";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 const StyledFilters = styled.aside`
   font-size: 1.5rem;
@@ -32,132 +34,169 @@ const StyledCheckbox = styled.div`
   padding-left: 2rem;
 `;
 
+const StyledFieldset = styled.fieldset`
+  margin-bottom: 1rem;
+`;
+
 function PlantsFilters() {
-  const handleClick = (value) => {};
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [activeFilters, setActiveFilters] = useState({
+    family: [],
+    size: [],
+    characteristics: [],
+  });
+
+  const handleCheckboxChange = (e) => {
+    const { name: group, value, checked } = e.target;
+
+    if (checked) {
+      const newFilters = { ...activeFilters };
+      newFilters[group] = [...newFilters[group], value];
+      setActiveFilters(newFilters);
+    } else {
+      const index = activeFilters[group].indexOf(value);
+      if (index !== -1) {
+        activeFilters[group].splice(index, 1);
+      }
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    for (const [group, value] of Object.entries(activeFilters)) {
+      if (value.length > 0) {
+        searchParams.set(group, value);
+      }
+    }
+    setSearchParams(searchParams.toString());
+  };
 
   return (
     <StyledFilters>
       <h3>Filters</h3>
-      <fieldset>
-        <legend>Family</legend>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="calathea"
-            name="family"
-            value="calathea"
-            onClick={() => handleClick("calathea")}
-          />
-          <label htmlFor="calathea">Calathea</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="ficus"
-            name="family"
-            value="ficus"
-            onClick={() => handleClick("ficus")}
-          />
-          <label htmlFor="ficus">Ficus</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="philodendron"
-            name="family"
-            value="philodendron"
-            onClick={() => handleClick("philodendron")}
-          />
-          <label htmlFor="philodendron">Philodendron</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="monstera"
-            name="family"
-            value="monstera"
-            onClick={() => handleClick("monstera")}
-          />
-          <label htmlFor="monstera">Monstera</label>
-        </StyledCheckbox>
-      </fieldset>
-      <fieldset>
-        <legend>Size</legend>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="m"
-            name="size"
-            value="m"
-            onClick={() => handleClick("m")}
-          />
-          <label htmlFor="m">M</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="l"
-            name="size"
-            value="l"
-            onClick={() => handleClick("l")}
-          />
-          <label htmlFor="l">L</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="xl"
-            name="size"
-            value="xl"
-            onClick={() => handleClick("xl")}
-          />
-          <label htmlFor="xl">XL</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="xxl"
-            name="size"
-            value="xxl"
-            onClick={() => handleClick("xxl")}
-          />
-          <label htmlFor="xxl">XXL</label>
-        </StyledCheckbox>
-      </fieldset>
-      <fieldset>
-        <legend>Characteristics</legend>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="airPurifying"
-            name="characteristics"
-            value="airPurifying"
-            onClick={() => handleClick("airPurifying")}
-          />
-          <label htmlFor="airPurifying">Air purifying</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="petFriendly"
-            name="characteristics"
-            value="petFriendly"
-            onClick={() => handleClick("petFriendly")}
-          />
-          <label htmlFor="petFriendly">Pet friendly</label>
-        </StyledCheckbox>
-        <StyledCheckbox>
-          <input
-            type="checkbox"
-            id="easy"
-            name="characteristics"
-            value="easy"
-            onClick={() => handleClick("easy")}
-          />
-          <label htmlFor="easy">Easy care</label>
-        </StyledCheckbox>
-      </fieldset>
-      <Button variation="secondary">Apply filters</Button>
+      <form onSubmit={handleSubmit}>
+        <StyledFieldset>
+          <legend>Family</legend>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="calathea"
+              name="family"
+              value="calathea"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="calathea">Calathea</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="ficus"
+              name="family"
+              value="ficus"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="ficus">Ficus</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="philodendron"
+              name="family"
+              value="philodendron"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="philodendron">Philodendron</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="monstera"
+              name="family"
+              value="monstera"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="monstera">Monstera</label>
+          </StyledCheckbox>
+        </StyledFieldset>
+        <StyledFieldset>
+          <legend>Size</legend>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="m"
+              name="size"
+              value="m"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="m">M</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="l"
+              name="size"
+              value="l"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="l">L</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="xl"
+              name="size"
+              value="xl"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="xl">XL</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="xxl"
+              name="size"
+              value="xxl"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="xxl">XXL</label>
+          </StyledCheckbox>
+        </StyledFieldset>
+        <StyledFieldset>
+          <legend>Characteristics</legend>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="airPurifying"
+              name="characteristics"
+              value="airPurifying"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="airPurifying">Air purifying</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="petFriendly"
+              name="characteristics"
+              value="petFriendly"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="petFriendly">Pet friendly</label>
+          </StyledCheckbox>
+          <StyledCheckbox>
+            <input
+              type="checkbox"
+              id="easy"
+              name="characteristics"
+              value="easy"
+              onChange={(e) => handleCheckboxChange(e)}
+            />
+            <label htmlFor="easy">Easy care</label>
+          </StyledCheckbox>
+        </StyledFieldset>
+        <Button variation="secondary">Apply filters</Button>
+      </form>
     </StyledFilters>
   );
 }
--- src/features/plants/PlantsList.jsx
+++ src/features/plants/PlantsList.jsx
@@ -5,6 +5,7 @@ import Pagination from "../../ui/Pagination";
 import Sort from "../../ui/Sort";
 import { useEffect, useState } from "react";
 import { usePlants } from "./usePlants";
+import { useSearchParams } from "react-router-dom";
 
 const StyledPlantsList = styled.section`
   background-color: var(--color-primary-50);
@@ -15,36 +16,11 @@ const StyledPlantsList = styled.section`
 
 function PlantsList() {
   const { isLoading, plants } = usePlants();
-  // const [sortBy, setSortBy] = useState("default");
-  // const [sortedPlants, setSortedPlants] = useState([]);
+  const [searchParams] = useSearchParams();
 
-  // const handleInputChange = (i) => {
-  //   setSortBy(i);
-  // };
+  const filters = Object.fromEntries([...searchParams]);
 
-  // const sortPlants = () => {
-  //   switch (sortBy) {
-  //     case "nameAscending":
-  //       console.log("nameA");
-  //       console.log(sortedPlants);
-  //       setSortedPlants(plants);
-  //       break;
-  //     case "nameDescending":
-  //       console.log("nameD");
-  //       setSortedPlants(plants);
-  //       break;
-  //     case "priceHigher":
-  //       console.log("priceH");
-  //       setSortedPlants(plants);
-  //       break;
-  //     case "priceLower":
-  //       console.log("priceL");
-  //       setSortedPlants(plants);
-  //       break;
-  //     default:
-  //       setSortedPlants(plants);
-  //   }
-  // };
+  let filteredPlants;
 
   if (isLoading) return <Spinner />;
 
